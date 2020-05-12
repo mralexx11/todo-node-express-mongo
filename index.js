c3f9cae9361c0b2b827638fc9021de0500a47fa5
Alex <42402960+mralexx11@users.noreply.github.com>
2020-05-12 19:10:15 +0300

    basic render

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
 const PORT = process.env.PORT || 3000
 
 const app = express()
-
-app.listen(PORT, () => {
-    console.log('Server has been started...');
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
 })
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(todoRoutes)
+
+async function start() {
+    try {
+        await mongoose.connect(
+            'mongodb+srv://alex:qwertypassword@cluster0-dxxhm.mongodb.net/todos', {
+                useNewUrlParser: true,
+                useFindAndModify: false
+            })
+        app.listen(PORT, () => {
+            console.log('Server has been started...');
+        })
+    } catch (err) {
+        console.log(err);
+
+    }
+}
+
+start()
---
+++ routes/todos.js
@@ -0,0 +1,19 @@
+const {Router} = require('express')
+const router = Router()
+
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Todos list',
+        isIndex: true
+    })
+})
+
+router.get('/create', (req, res) => {
+    res.render('create', {
+        title: 'Create todo',
+        isCreate: true
+    })
+})
+
+
+module.exports = router
